88d42b83a6514582a5b71f085412b3aa2072c517
Abdul Waris <maiyaleeg223@gmail.com>
2024-07-19 19:59:06 -0400

    updated resume

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -4,7 +4,7 @@ const Resume = () => {
   return (
     <div className="text-center mt-10">
       <h1 className="text-3xl font-bold mb-6"></h1>
-      <object data="/resume.pdf" type="application/pdf" width="100%" height="800px">
+      <object data="/resume2024.pdf" type="application/pdf" width="100%" height="800px">
         <p>It appears you don&apos;t have a PDF plugin for this browser. No biggie... you can <a href="/resume.pdf" download="resume.pdf">click here to download the PDF file.</a></p>
       </object>
     </div>
