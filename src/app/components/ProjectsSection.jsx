31c33c9f533c8d94972a5654232cf44ba9db2f7a
Abdul Waris <maiyaleeg223@gmail.com>
2024-08-17 20:26:56 -0400

    trying smaller gif sizes

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -27,7 +27,7 @@ const projectsData = [
     id: 6,
     title: "Vibr",
     description: "Vibr is Full-Stack web application. Where a user can connect & touch base with family/friends with notifications included",
-    image: "images/projects/Vibr.gif", 
+    image: "images/projects/vibr.gif", 
     tag: ["All", "Web"],
     gitUrl: "https://github.com/Waris-95/VibR",
     previewUrl: "https://vibr.onrender.com/",
